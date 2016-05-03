7a0c0fb0e234bce19cc9fef7918c5c86c1e34b0d
Lennart Buit <lennartbuit@gmail.com>
2016-05-03 16:36:01 +0200

    Made application more aware of ^C

--- websocket/main.py
+++ websocket/main.py
@@ -1,5 +1,6 @@
 from asyncio import Queue
-from threading import Thread
+import threading
+import time
 
 from serial_client import SerialClient
 from websocket_server import *
@@ -16,20 +17,21 @@ def main():
     ledbar_server = create_ledbar_server(command_queue)
 
     # Run the websocket server in a seperate thread
-    ledbar_server_thread = Thread(target=ledbar_server.run_forever)
+    ledbar_server_thread = threading.Thread(target=ledbar_server.run_forever)
+    ledbar_server_thread.setDaemon(True)
 
     # Run the serial client in a seperate thread
-    serial_client_thread = Thread(target=serial_client.run_forever)
+    serial_client_thread = threading.Thread(target=serial_client.run_forever)
+    serial_client_thread.setDaemon(True)
 
     ledbar_server_thread.start()
     serial_client_thread.start()
 
     try:
-        ledbar_server_thread.join()
-        serial_client_thread.join()
+        while threading.active_count() > 0:
+            time.sleep(0.1)
     except:
-        ledbar_server.stop()
-        serial_client.stop()
+        print("Shutting down...")
 
 if __name__ == "__main__":
     main()
