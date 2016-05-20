cc0ae258c448ca44d27bbf8a975e809034172788
AVeenstra <AVeenstra@users.noreply.github.com>
2016-05-20 19:16:58 +0200

    Update newledstrip.ino

--- arduino/newledstrip/newledstrip.ino
+++ arduino/newledstrip/newledstrip.ino
@@ -7,10 +7,27 @@
 #define IA_GREEN 66
 #define IA_BLUE 138
 
+#define OFFSET_BLUE   40
+#define OFFSET_RED    (OFFSET_BLUE * IA_RED   / IA_BLUE)
+#define OFFSET_GREEN  (OFFSET_BLUE * IA_GREEN / IA_BLUE)
+
+// Moet 2**n - 1 zijn
+#define INTERVAL 255
+
+#define CORRECTEDINTERVAL (PI * 2 / INTERVAL)
+
 Adafruit_NeoPixel strip = Adafruit_NeoPixel(NUMPIXELS, PIN, NEO_GRB + NEO_KHZ800);
-uint8_t pixels[NUMPIXELS*3];
+uint8_t pixels[NUMBYTES];
+uint8_t sine[INTERVAL*3];
+boolean local_animate = false;
+uint8_t counter = 0;
 
 void setup() {
+  for (int i = 0; i < NUMPIXELS; ++i) {
+    sine[i*3]   = IA_RED   - OFFSET_RED   * cos(i * CORRECTEDINTERVAL);
+    sine[i*3+1] = IA_GREEN - OFFSET_GREEN * cos(i * CORRECTEDINTERVAL);
+    sine[i*3+2] = IA_BLUE  - OFFSET_BLUE  * cos(i * CORRECTEDINTERVAL);
+  }
   Serial.begin(115200);
   Serial.setTimeout(50);
   strip.begin();
@@ -18,25 +35,40 @@ void setup() {
 }
 
 void loop() {
-  Serial.write(255);
+  Serial.write(counter);
   if(Serial.readBytes(pixels, NUMBYTES) == NUMBYTES) {
+    local_animate = false;
     for (int i = 0; i < NUMPIXELS; ++i) {
       strip.setPixelColor(i, strip.Color(pixels[i*3], pixels[i*3+1], pixels[i*3+2]));
     }
+  } else if (local_animate) {
+    counter = (counter + 1) & INTERVAL;
+    for (int i = 0; i < NUMPIXELS; ++i) {
+      int j = ((i+counter) & INTERVAL) * 3;
+      strip.setPixelColor(i, strip.Color(sine[j], sine[j+1], sine[j+2]));
+    }
   } else {
+    local_animate = true;
     for (int i = 0; i < NUMPIXELS; ++i) {
       uint8_t& r = pixels[i*3];
       uint8_t& g = pixels[i*3+1];
       uint8_t& b = pixels[i*3+2];
+      int j = ((i+counter) & INTERVAL) * 3;
+      uint8_t& rn = sine[j];
+      uint8_t& gn = sine[j+1];
+      uint8_t& bn = sine[j+2];
 
-      if (r != IA_RED) {
-        r = pixels[i*3] =   r + (((IA_RED   - r) >> 8) | 1);
+      if (r != rn) {
+        r += ((rn - r) >> 8) | 1;
+        local_animate = false;
       }
-      if (g != IA_GREEN) {
-        g = pixels[i*3+1] = g + (((IA_GREEN - g) >> 8) | 1);
+      if (g != gn) {
+        g += ((gn - g) >> 8) | 1;
+        local_animate = false;
       }
-      if (b != IA_BLUE) {
-        b = pixels[i*3+2] = b + (((IA_BLUE  - b) >> 8) | 1);
+      if (b != bn) {
+        b += ((bn - b) >> 8) | 1;
+        local_animate = false;
       }
       
       strip.setPixelColor(i, strip.Color(r, g, b));
